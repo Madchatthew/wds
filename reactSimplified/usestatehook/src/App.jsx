f7b3917ab5e5ac35d9f1c98e842da0ffdccc6692
Madchatthew <chad.jessen@gmail.com>
2023-05-04 22:36:00 -0500

    useState and how to use it

--- reactSimplified/usestatehook/src/App.jsx
+++ reactSimplified/usestatehook/src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react"
+import { Counter } from "./Counter"
 
 function App() {
   const [name, setName] = useState("Chad")
+  const [age, setAge] = useState(46)
+  // const [ person, setPerson] = useState({ name: "Chad", age: 46 }) - It is best to separate the variables to make it easier to change values
 
-  function handleClick() {
-    setName("More Cowbell")
-  }
-  return <h1 onClick={handleClick}> Hi {name}</h1>
+  // function handleClick() {
+  //   setName("More Cowbell")
+  //   setAge(age + 1)
+  // }
+
+  return <Counter />
 }
 
 export default App
+
+{
+  /* <h1 onClick={handleClick}>
+       Hi {name} {age}
+      </h1> */
+}
