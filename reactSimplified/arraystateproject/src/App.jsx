33d86b8328d9a886e9425472cc61c376405a076c
Madchatthew <chad.jessen@gmail.com>
2023-05-11 21:02:16 -0500

    start of arraystateproject

---
+++ reactSimplified/arraystateproject/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+function App() {
+  const [array, setArray] = useState(["A", "B", "C"])
+
+  function removeFirstElement()
+
+  return
+  <div>{array.join(", ")}
+  <button onClick={removeFirstElement}>Remove First Element</button>
+  </div>
+}
+
+export default App
